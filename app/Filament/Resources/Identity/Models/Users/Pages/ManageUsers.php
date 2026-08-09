e78a262ad2ef0af6f4681192925c19aa63f13c45
Faizpi <faizbintang1244@gmail.com>
2026-08-09 13:40:14 +0700

    feat: tambahkan halaman manajemen pengguna

---
+++ app/Filament/Resources/Identity/Models/Users/Pages/ManageUsers.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources\Identity\Models\Users\Pages;
+
+use App\Domain\Identity\Actions\ManageUsers as ManageUsersAction;
+use App\Filament\Resources\Identity\Models\Users\UserResource;
+use App\Models\User;
+use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ManageRecords;
+
+final class ManageUsers extends ManageRecords
+{
+    protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()->label('Pengguna Baru')->using(fn (array $data): User => app(ManageUsersAction::class)->create(auth()->user(), $data)),
+        ];
+    }
+}
